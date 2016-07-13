ccddbd251076cf825eb547b949a8bb1b1938ba73
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2016-07-13 16:54:24 -0600

    finished first problem

--- higher-order-challenge.js
+++ higher-order-challenge.js
@@ -1,18 +1,43 @@
-any([ 8, 9, 10, 11 ], greaterThan10);
-// surprise()()
-// filter([ 1, 2, 3, 4 ], onlyOdd)
+//console.log(any([ 8, 9, 10, 11 ], greaterThan10));
+//surprise()()
+//filter([ 1, 2, 3, 4 ], onlyOdd)
 // sumTwoNumbers(3)(9)
 
-function any(array) {
-  for (var i = 0; i < array.length; i++) {
-    return array[i]
-  }
-}
+// any and greaterThan10 definitions
 
-function greaterThan10 (num) {
-  if (num < 10) {
-    return false
-  } else {
-    return true
+// function any(arr, cbFn) {
+//   for (var i = 0; i < arr.length; i++) {
+//     if (greaterThan10(arr[i])) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
+//
+// function greaterThan10 (num) {
+//   if (num > 10) {
+//     return true;
+//   } else {
+//     return false
+//   }
+// }
+
+// surprise definition
+
+
+// filter and onlyOdd definition
+
+// function filter(arr, cbFn) {
+//
+// }
+
+function onlyOdd (num) {
+    if (num % 2 !== 0) {
+      
+    }
   }
-}
+
+
+console.log(onlyOdd([1, 2, 3, 4]));
+
+// sumTwoNumbers definition
